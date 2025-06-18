18864d0e6911fa3140a81ae7703eec080774bc5a
iamvishalchandra <vishal.chandra75@gmail.com>
2025-06-18 20:51:00 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-2xl text-primary">Hello Androids World!</Text>
+    </View>
+  );
+}
